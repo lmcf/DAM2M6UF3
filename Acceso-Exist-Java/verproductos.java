00f1f8f42ccfbd6a0d75fb828730bbf20f036eb1
Caldeiro, Luis M <caldeiro@protonmail.com>
2019-04-02 21:58:54 +0200

    Acceso Java - Ex 1

---
+++ Acceso-Exist-Java/verproductos.java
@@ -0,0 +1,33 @@
+import javax.xml.xquery.*;
+import net.xqj.exist.ExistXQDataSource;
+
+/*
+ * Problemas XQJ_1
+ * Ejercicio 1: Descarga la API y prueba el ejemplo anterior
+ */
+
+public class verproductos {
+	public static void main(String[] args) {
+		try {
+			XQDataSource server = new ExistXQDataSource();
+			server.setProperty("serverName", "192.168.56.102");
+			server.setProperty("port", "8080");
+			server.setProperty("user", "admin");
+			server.setProperty("password", "austria");
+			XQConnection conn = server.getConnection();
+			XQPreparedExpression consulta;
+			XQResultSequence resultado;
+			
+			consulta = conn.prepareExpression("for $pr in doc('nueva/productos.xml')/productos/produc return $pr");
+			resultado = consulta.executeQuery();
+			
+			while (resultado.next()) {
+				System.out.println(resultado.getItemAsString(null));
+			}
+			conn.close();
+			
+		} catch (XQException ex) {
+			System.out.println("Error al operar" + ex.getMessage());
+		}
+	}
+}
